0850e98aee2ddfda611b7a40d377819c69f30ac4
xuechenfei <xuechenfei@ahaschool.com>
2019-07-23 19:00:31 +0800

    参数校验

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# dom-visible-observer
-观察dom元素是否在可视窗口内~
+# baselazyload
+适用于图片懒加载
+
+```
+import lazyload form 'baselazyload'
+
+lazyload({
+    container
+    el,
+    src,
+    threshold
+})
+```
+
+## container
+懒加载的图片所在的滚动容器 默认是document
+
+## el
+监听的图片元素，必须是img标签
+
+## scr
+需要加载的真实图片url
+
+## threshold
+差值 默认为0
--- index.js
+++ index.js
@@ -4,10 +4,19 @@ import './index.css'
 function lazyload({
     container = document,
     el,
-    defaultSrc,
     src,
     threshold = 0
 }) {
+    const errorMsg = checkParamIterator(
+        checkElTag,
+        checkImgSrc,
+        checkThreshold
+    )({ el, src, threshold })
+
+    if (errorMsg) return
+
+    let defaultSrc = el.src
+
     return visibleObserver({
         container,
         el,
@@ -19,13 +28,13 @@ function lazyload({
                 el.style.opacity = 0
 
                 setTimeout(() => {
-                    el.className += ' lazyload-fadein'
+                    el.className +=
+                        ' lazyload-fadein'
                     el.style.opacity = 1
                 }, 50)
             })
         },
-        hide() {
-        }
+        hide() {}
     })
 }
 
@@ -33,6 +42,45 @@ function proxyImage(src, cb) {
     let Img = new Image()
     Img.src = src
     Img.onload = cb
+}
+
+function checkElTag(param) {
+    const { el } = param
+    if (
+        !el ||
+        !el.tagName ||
+        el.tagName.toLowerCase() !== 'img'
+    ) {
+        return (
+            'missing el parameter or el is not a ' +
+            new Image().toString()
+        )
+    }
+}
+function checkImgSrc(param) {
+    const { src } = param
+    if (
+        !src ||
+        !/^(http:|data:|https:)/.test(src)
+    ) {
+        return 'missing src parameter or src is not a correct Picture address'
+    }
+}
+function checkThreshold(param) {
+    if (typeof param.threshold !== 'number') {
+        return 'threshold must be a number'
+    }
+}
+function checkParamIterator() {
+    let args = [].slice.call(arguments)
+    let msg = '';
+    return function(param) {
+        for (let i = 0; i < args.length; i++) {
+            msg = args[i](param)
+            msg && console.error(msg)
+        }
+        return msg;
+    }
 }
 
 export default lazyload
